3500ad8a07cb011c56684eb7c25c0ff2d30c7702
Gustavo Percoski <gustavopercoski2@gmail.com>
2026-02-22 02:12:26 -0300

    refactor: melhorando estrutura do repositório com seções

---
+++ Section01_EstruturaBasicaCSharp/PrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// Primeiro programa em C#. O "using System" permite usar o Console.
+// O programa começa no método Main e o Console.WriteLine mostra a mensagem na tela.
+// Usei esse formato mais completo pra entender melhor como funciona a estrutura básica.
